ca59fb88e83b754361c0045daacf57dffdec276b
Admin <trongtin100@gmail.com>
2022-08-23 16:05:44 +0700

    feature-02

---
+++ EX-16-04-HuongDoiTuongKhungTronCanvas.js
@@ -0,0 +1,29 @@
+function Hinhtron(x, y, bankinh){
+    this.x=x;
+    this.y=y;
+    this.bankinh = bankinh;
+}
+
+function getRandomHex(){
+    return Math.floor(Math.random()*255);
+}
+
+function getRandomColor(){
+    var red = getRandomHex();
+    var green = getRandomHex();
+    var blue = getRandomHex();
+        return "rgb(" + red + "," + blue + "," + green + ")";
+}
+
+function taoHinhtron(){
+    var tin = document.getElementById("myFrame").getContext("2d");
+    var bankinh = Math.floor(Math.random()*80);
+    var color = getRandomColor();
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    var vongtron= new Hinhtron(x, y, bankinh);
+    tin.beginPath();
+    tin.arc(vongtron.x, vongtron.y, vongtron.bankinh, 0, 2 * Math.PI );
+    tin.fill();
+};
+taoHinhtron();
